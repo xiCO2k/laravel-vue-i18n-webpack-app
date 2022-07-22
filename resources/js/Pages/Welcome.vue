fd88ba7c2f80fee84216d0870b35a457cff65dc9
Francisco Madeira <xico2k@gmail.com>
2022-07-22 11:52:35 +0100

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+import { trans } from 'laravel-vue-i18n';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+
+const test = trans('auth.failed')
+</script>
+
+<template>
+        {{ $t('auth.failed') }}
+        {{ $t('auth.password') }}
+        {{ test }}
+</template>
---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+require('./bootstrap');
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+import { i18nVue } from 'laravel-vue-i18n';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => require(`./Pages/${name}.vue`),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .use(i18nVue, {
+                resolve: (lang) => import(`../../lang/${lang}.json`)
+            })
+            .mixin({ methods: { route } })
+            .mount(el);
+    },
+});
+
+InertiaProgress.init({ color: '#4B5563' });
